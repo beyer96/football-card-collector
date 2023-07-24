72a9aebf9ea66813ca1f23a5d62ba2ec333d03c4
Lukas Beyer <lukas.beyer@juicymo.cz>
2023-07-25 00:23:43 +0200

    Remove vite-plugin-mkcert since its now redundant - problem was solved on server

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    mkcert() // TODO: Probably need to adjust this only for develop environment
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  server: {
-    https: true
   }
 })
