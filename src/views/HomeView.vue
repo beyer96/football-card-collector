9c532c9065cd7e706524ade18223cf24a6b16e86
Lukas Beyer <lukas.beyer@juicymo.cz>
2023-10-09 22:30:40 +0200

    Render 'Hot stuff' section in HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,15 +10,47 @@
       <div>
         <button type="button" @click.prevent="testFootballAPI">Test Football API endpoint (fetch czech clubs)</button>
       </div>
+
+      <CardsSectionComponent
+        header="Hot Stuff"
+        :players="cardStore.hotStuffCards"
+      />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { getRefreshToken } from '../utils/auth';
-import { useUserStore } from '../stores/UserStore';
+import { onMounted } from 'vue';
 
+import { useAppStore } from '../stores/AppStore';
+import { useUserStore } from '../stores/UserStore';
+import { useCardStore } from '../stores/CardStore';
+
+import CardsSectionComponent from '../components/CardsSectionComponent.vue';
+
+import { API_BASE_URL } from '../utils/constants';
+import { getRefreshToken } from '../utils/auth';
+
+const appStore = useAppStore();
 const userStore = useUserStore();
+const cardStore = useCardStore();
+
+onMounted(async () => {
+  appStore.setIsLoading(true);
+
+  try {
+    if (!cardStore.hotStuffCards.length) {
+      const response = await fetch(`${API_BASE_URL}/players/random`);
+      const { data } = await response.json();
+
+      cardStore.setHotStuffCards(data.players);
+    }
+  } catch (err) {
+    console.log(err.message);
+  } finally {
+    appStore.setIsLoading(false);
+  }
+});
 
 const testProtectedEndpoint = async () => {
   const tokenExpiration = sessionStorage.getItem("token-expiration");
