b76d2d954f51419006ef12a73a03e2bd2649a741
Lukas Beyer <lukas.beyer@juicymo.cz>
2023-10-09 22:27:24 +0200

    Create API_BASE_URL constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,3 @@
 export const AUTH_SERVER_BASE_URL: string = import.meta.env.VITE_AUTH_SERVER_BASE_URL;
 export const SERVER_BASE_URL: string = import.meta.env.VITE_SERVER_BASE_URL;
+export const API_BASE_URL: string = "http://localhost:8000/api/v1";
